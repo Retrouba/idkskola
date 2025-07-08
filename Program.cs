5c28699605acc1eaa4bdcc6e885927ede6938781
Kuba J <k.janovsky6210@gmail.com>
2025-07-08 15:47:08 +0200

    bombagiit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Napis 3 cisla");
+int cislo1 = int.Parse(Console.ReadLine());
+int cislo2 = int.Parse(Console.ReadLine());
+int cislo3 = int.Parse(Console.ReadLine());
+Console.WriteLine(cislo1 + " " + cislo2 + " " + cislo3);
